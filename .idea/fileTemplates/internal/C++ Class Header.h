97695428046c8d87c06223fdf437c07b1b0f3fbc
Richard Hodges <hodges.r@gmail.com>
2022-05-01 21:00:46 +0200

    add file templates for clion

---
+++ .idea/fileTemplates/internal/C++ Class Header.h
@@ -0,0 +1,13 @@
+#parse("C File Header.h")
+#[[#ifndef]]# ${INCLUDE_GUARD}
+#[[#define]]# ${INCLUDE_GUARD}
+
+${NAMESPACES_OPEN}
+
+struct ${NAME} {
+
+};
+
+${NAMESPACES_CLOSE}
+
+#[[#endif]]# //${INCLUDE_GUARD}
